4627a27bd9e40fe8ff8e70d73d37ec608b7935d5
Chetnabahuriya <chetnabahuriya@gmail.com>
2025-07-27 20:08:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', eu-north-1_8iBpT5eF9
+        userPoolClientId: '', 6190e5p26ula54jvjeb62o72i
+        region: '' eu-north-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
